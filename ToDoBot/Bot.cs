8ca111e4a68613253bcc839fe2cd189f5df3d163
Michael Spiegel <michaelspiegel@live.at>
2023-08-14 22:28:00 +0200

    Basic setup of the bot

---
+++ ToDoBot/Bot.cs
@@ -0,0 +1,45 @@
+using Discord;
+using Discord.WebSocket;
+
+namespace ToDoBot;
+
+public class Bot
+{
+    private readonly string _token;
+    private readonly ulong _channelId;
+    private readonly string _calendarUrl;
+    private readonly DiscordSocketClient _client;
+
+    public Bot(string token, ulong channelId, string calendarUrl)
+    {
+        _token = token;
+        _channelId = channelId;
+        _calendarUrl = calendarUrl;
+        _client = new DiscordSocketClient();
+    }
+
+    public async Task Start()
+    {
+        _client.Log += LogAsync;
+        _client.Ready += ReadyAsync;
+
+        await _client.LoginAsync(TokenType.Bot, _token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+    
+    private Task ReadyAsync()
+    {
+        Console.WriteLine("Bot is ready!");
+        
+        return Task.CompletedTask;
+    }
+    
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log);
+        
+        return Task.CompletedTask;
+    }
+}
---
+++ ToDoBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDoBot;
+
+public class Program
+{
+    private readonly IConfigurationRoot _configuration = CreateConfiguration();
+
+    static void Main()
+        => new Program().RunAsync().GetAwaiter().GetResult();
+    
+    private static IConfigurationRoot CreateConfiguration()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.json")
+            .Build();
+
+        return config;
+    }
+    
+    private async Task RunAsync()
+    {
+        // Read values from the configuration
+        var token = _configuration.GetSection("AppSettings").GetSection("BotToken").Value;
+        var channelId = Convert.ToUInt64(_configuration.GetSection("AppSettings").GetSection("ChannelId").Value);
+        var calendarUrl = _configuration.GetSection("AppSettings").GetSection("CalendarUrl").Value;
+
+        if (token == null || calendarUrl == null)
+        {
+            Console.WriteLine("Could not read values from appsettings.json");
+            return;
+        }
+        
+        var bot = new Bot(token, channelId, calendarUrl);
+        await bot.Start();
+    }
+}
