d3c5c3d0886cbdcea00205c31feda6e790eef563
Michael Spiegel <michaelspiegel@live.at>
2023-08-17 19:54:38 +0200

    Finished porting bot from python to CSharp

--- ToDoBot/Bot.cs
+++ ToDoBot/Bot.cs
@@ -10,6 +10,7 @@ public class Bot
     private readonly string _calendarUrl;
     private readonly DiscordSocketClient _client;
     private readonly ICalendarService _calendarService;
+    private Timer _dailyTimer;
 
     public Bot(string token, ulong channelId, string calendarUrl, ICalendarService calendarService)
     {
@@ -35,6 +36,8 @@ public class Bot
     {
         Console.WriteLine("Bot is ready!");
         
+        SetupDailyTimer();
+        
         return Task.CompletedTask;
     }
     
@@ -43,5 +46,60 @@ public class Bot
         Console.WriteLine(log);
         
         return Task.CompletedTask;
+    }
+    
+    private void SetupDailyTimer()
+    {
+        // Set up the timer to execute the SendToDos method
+        _dailyTimer = new Timer(
+            async _ => await SendToDos(),
+            null,
+            TimeSpan.Zero, // Initial delay (start immediately)
+            TimeSpan.FromHours(24) // Repeat every 24 hours
+        );
+    }
+    
+    private async Task SendToDos()
+    {
+        var today = DateTime.Now.DayOfWeek;
+
+        if (today != DayOfWeek.Monday && today != DayOfWeek.Wednesday && today != DayOfWeek.Friday) return;
+
+        if (_client.GetChannel(_channelId) is not IMessageChannel channel) return;
+        
+        var calendarData = await _calendarService.DownloadCalendarAsync(_calendarUrl);
+        var toDoItems = _calendarService.ParseCalendarDataToToDoItem(calendarData);
+        
+        var startDate = DateTime.Today;
+        var endDate = startDate.AddDays(7);
+        
+        // Filter the ToDo items based on the date range
+        var toDosWithinNext7Days = toDoItems.Where(todo => 
+            todo.SubmissionDate >= startDate && todo.SubmissionDate <= endDate
+        ).ToList();
+        
+        var embed = CreateEmbed(toDosWithinNext7Days);
+        
+        await channel.SendMessageAsync("", false, embed);
+    }
+    
+    private Embed CreateEmbed(List<ToDoItem> toDoItems)
+    {
+        var embedBuilder = new EmbedBuilder
+        {
+            Title = "ToDos",
+            Description = "The ToDos for the next 7 days",
+            Color = Color.Blue
+        };
+
+        foreach (var toDoItem in toDoItems)
+        {
+            embedBuilder.AddField("Assignment", toDoItem.Title, true);
+            embedBuilder.AddField("Submission Date", toDoItem.SubmissionDate.ToString("dd.MM.yyyy"), true);
+            embedBuilder.AddField("Submission Time", toDoItem.SubmissionDate.ToString("HH:mm:ss"), true);
+            embedBuilder.AddField("\u200b", "\u200b"); // Empty field to place other two fields next to each other
+        }
+        
+        return embedBuilder.WithCurrentTimestamp().Build();
     }
 }
